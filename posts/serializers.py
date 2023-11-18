36551597dc08e391023f1040bb7f8c269af8a35d
Sevastian Bajenov <sevabaj@gmail.com>
2023-11-18 17:54:28 +0200

    Add post serializers.

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,11 +3,25 @@ from .models import Post
 
 from polls.serializers import PollSerializer
 from polls.models import Poll
+from tags.serializers import TagSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
     polls_attached = PollSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ['title', 'body', 'timestamp', 'polls_attached']
+        fields = ['title', 'body', 'timestamp', 'tags', 'polls_attached']
+
+
+class CreatePostSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Post
+        fields = ['title', 'body', 'timestamp', 'tags']
+
+
+class CreatePostsSerializer(serializers.Serializer):
+    posts = CreatePostSerializer(many=True)
--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework import response, status
 
 from .models import Post
+from .serializers import CreatePostsSerializer
 
 root_pass_header_name = 'X-Password'
 
@@ -28,10 +29,18 @@ def add_posts(request):
             status=status.HTTP_403_FORBIDDEN
         )
     
-    
+    serializer = CreatePostsSerializer(data=request.data)
+    if serializer.is_valid():
+        for post in serializer.validated_data.get('posts', []):
+            Post.objects.create(post)
 
-    return response.Response(
-        {'message': "Poll successfully approved!"},
-        status=status.HTTP_200_OK
-    )
+        return response.Response(
+            {'message': "Poll successfully approved!"},
+            status=status.HTTP_200_OK
+        )
+    else:
+        return response.Response(
+            {'message': serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
